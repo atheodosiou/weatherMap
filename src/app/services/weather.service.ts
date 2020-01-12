bedb54f5dca1921e2bebbe0a0224092a5c9c696a
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2020-01-12 23:23:51 +0200

    Add some styling

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -13,7 +13,7 @@ export class WeatherService {
   private apiKey = '52259bbf96f161eca25b0162290f1c24';
 
   getCurrentWeatherByCoords(lat: number, lon: number): Observable<any> {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${this.apiKey}`);
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${this.apiKey}&lang=el`);
   }
 
   getCities(): Observable<any> {
